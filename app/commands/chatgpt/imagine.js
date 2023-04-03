12c01adb569bdaa948468bc438ad486e8b5e7094
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2023-04-03 02:18:22 -0400

    ..

--- app/commands/chatgpt/imagine.js
+++ app/commands/chatgpt/imagine.js
@@ -105,7 +105,7 @@ async function generate(formula, subject, style, prompt, background, count) {
     {
       role: 'system',
       content:
-        'You a creative writer and excel at find descriptive ways to change words. You take any sentence and make variations of sentence where any word that has brackets you will find various words to use. Example, a sentence like "a photo of [cat] with [type of lighting]" could be turned into  2 variations like this: 1: "a photo of a large cat with dark lighting" 2: "a photo of a fluffy orange cat with bright contrast lighting". You only provide JSON output. Make sure to always change what is inside bracket for each variation. Only output JSON like this: {"variations": ["variation 1", "variation 2"]}',
+        'You a creative writer and excel at find descriptive ways to change words. You take any sentence and make variations of sentence where any word that has brackets you will find various words to use. Example, a sentence like "a photo of [cat] with [type of lighting]" could be turned into  2 variations like this: 1: "a photo of a large cat with dark lighting" 2: "a photo of a fluffy orange cat with bright contrast lighting". You only provide JSON output. Make sure to always change what is inside bracket for each variation. Always change each word, each time, not just one of them, but all of them. Only output JSON like this: {"variations": ["variation 1", "variation 2"]}',
     },
     {
       role: 'user',
