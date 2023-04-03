50dcfc959cc04eb8f830b50ef3db68a6c856fa4e
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2023-04-03 01:59:47 -0400

    ..

---
+++ app/commands/chatgpt/imagine.js
@@ -0,0 +1,142 @@
+/*
+ *   Copyright (c) 2023 Wynter Jones
+ *   All rights reserved.
+ */
+
+const { SlashCommandBuilder } = require('discord.js')
+const { Configuration, OpenAIApi } = require('openai')
+const configuration = new Configuration({
+  apiKey: process.env.CHATGPT_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+const { formulas } = require('../../engine/midjourney-formulas.json')
+const options = Object.entries(formulas).map(([key, value]) => {
+  return {
+    name: key,
+    value: key,
+  }
+})
+
+const data = new SlashCommandBuilder()
+  .setName('imagine')
+  .setDescription('Auto imagine with ChatGPT')
+
+data.addStringOption(option =>
+  option
+    .setName('formula')
+    .setDescription(`Pick a formula to use.`)
+    .setRequired(true)
+)
+
+options.forEach(cat => {
+  data.options[0].addChoices(cat)
+})
+
+data.addStringOption(option =>
+  option
+    .setName('count')
+    .setDescription('How many variations?')
+    .setRequired(true)
+)
+
+data.addStringOption(option =>
+  option
+    .setName('subject')
+    .setDescription('What is the subject of the image?')
+    .setRequired(true)
+)
+
+data.addStringOption(option =>
+  option
+    .setName('style')
+    .setDescription('What is the style of the image?')
+    .setRequired(true)
+)
+
+data.addStringOption(option =>
+  option
+    .setName('prompt')
+    .setDescription(
+      'Advanced customization of the prompt, see /help to learn more.'
+    )
+)
+
+module.exports = {
+  data: data,
+  async execute(interaction) {
+    const formula = interaction.options.getString('formula')
+    const subject = interaction.options.getString('subject')
+    const style = interaction.options.getString('style')
+    const prompt = interaction.options.getString('prompt')
+    const count = interaction.options.getString('count')
+
+    if (!formula) return interaction.reply('Please provide a formula.')
+    if (!subject) return interaction.reply('Please provide a subject.')
+    if (!style) return interaction.reply('Please provide a style.')
+    if (!count) return interaction.reply('Please provide a count.')
+
+    await interaction.deferReply()
+    await interaction.editReply(
+      await generate(formula, subject, style, prompt, count)
+    )
+  },
+}
+
+async function generate(formula, subject, style, prompt, count) {
+  const selectedFormula = formulas[formula]
+  const result = replacePlaceholders(selectedFormula, prompt)
+  const content = result
+    .replace('[subject]', `[${subject}]`)
+    .replace('[style]', `[${style}]`)
+
+  const conversation = [
+    {
+      role: 'system',
+      content:
+        'You are a variation wizard. You take any sentence and make variations of sentence where any word that has brackets you will find various words to use. Example, a sentence like "a photo of [cat] with [type of lighting]" could be turned into  2 variations like this: 1: "a photo of a large cat with dark lighting" 2: "a photo of a fluffy orange cat with bright contrast lighting". You only provide JSON output. Make sure to always change what is inside bracket for each variation. Only output JSON like this: {"variations": ["variation 1", "variation 2"]}',
+    },
+    {
+      role: 'user',
+      content: `I want ${count} variations of the following as JSON (never append a period at end of sentence or capitalize, keep all lower case): "${content}"`,
+    },
+  ]
+
+  const chatgpt = await openai
+    .createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: conversation,
+    })
+    .catch(error => {
+      return (
+        'Sorry, ran into trouble with the OpenAI API. Error Message: ' + error
+      )
+    })
+
+  const replyJSON = JSON.parse(chatgpt.data.choices[0].message.content)
+
+  let response = ''
+  for (let i = 0; i < replyJSON.variations.length; i++) {
+    response += `\> /imagine prompt: ${replyJSON.variations[i]}\n\n`
+  }
+
+  return response
+}
+
+function replacePlaceholders(template, keyValueString) {
+  const keyValueRegex = /\[(\w+(?:\s+\w+)*)=(\w+)\]/g
+  const keyValuePairs = {}
+
+  let keyValueMatch
+  while ((keyValueMatch = keyValueRegex.exec(keyValueString)) !== null) {
+    const key = keyValueMatch[1]
+    const value = keyValueMatch[2]
+    keyValuePairs[key] = value
+  }
+
+  const regex = /\[([^\]]+)\]/g
+  const result = template.replace(regex, (match, word) => {
+    return keyValuePairs[word] ? `[${keyValuePairs[word]}]` : match
+  })
+
+  return result
+}
--- app/old/upload.js
+++ app/old/upload.js
@@ -4,30 +4,21 @@
  */
 
 const cloudinary = require('cloudinary')
-const parseInputString = require('../utils/parseInput')
 
 async function upload(message) {
-  await message.channel.sendTyping()
-
-  const content = message.content.replace('!!upload ', '')
-  const parsedData = parseInputString(content)
-
-  console.log(parsedData)
-
-  // const file =
-  //   'https://media.discordapp.net/ephemeral-attachments/1071927262099804202/1092269361789673492/wynterjones_A_photograph_of_a_Maine_Coon_cat_climbing_a_tree_wi_094ad523-1b60-4a31-a4a0-ca45624dd6c1.png?width=2324&height=1302'
-  // const innerFolder = 'photos'
-  // const folder = `${process.env.CLOUDINARY_FOLDER}/${innerFolder}`
-
-  // cloudinary.v2.uploader
-  //   .unsigned_upload(file, process.env.CLOUDINARY_UPLOAD_PRESET, {
-  //     folder: folder,
-  //   })
-  //   .then(response => {
-  //     message.channel.send(
-  //       `Uploaded to Cloudinary -> **/${folder}**\n ${response.secure_url}`
-  //     )
-  //   })
+  const file =
+    'https://media.discordapp.net/ephemeral-attachments/1071927262099804202/1092269361789673492/wynterjones_A_photograph_of_a_Maine_Coon_cat_climbing_a_tree_wi_094ad523-1b60-4a31-a4a0-ca45624dd6c1.png?width=2324&height=1302'
+  const innerFolder = 'photos'
+  const folder = `${process.env.CLOUDINARY_FOLDER}/${innerFolder}`
+  cloudinary.v2.uploader
+    .unsigned_upload(file, process.env.CLOUDINARY_UPLOAD_PRESET, {
+      folder: folder,
+    })
+    .then(response => {
+      message.channel.send(
+        `Uploaded to Cloudinary -> **/${folder}**\n ${response.secure_url}`
+      )
+    })
 }
 
 module.exports = upload
