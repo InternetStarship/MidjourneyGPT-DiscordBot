f8dd83b64b7dc1bdaa6cb7478d39c0fbf4841a45
Wynter Jones <34955844+WynterJones@users.noreply.github.com>
2023-04-04 01:57:36 -0400

    ..

--- app/commands/chatgpt/formula.js
+++ app/commands/chatgpt/formula.js
@@ -33,10 +33,6 @@ module.exports = {
 
     await interaction.deferReply()
     await interaction.editReply(await generate(draft))
-
-    console.log(chatgpt.data.choices[0].message.content)
-
-    interaction.reply(`Awesome, here is your possible formulas.`)
   },
 }
 
@@ -44,7 +40,7 @@ const generate = async draft => {
   const conversation = [
     {
       role: 'system',
-      content: `You help create text prompts for an AI text-to-image software called Midjourney. You will take concepts and make 4 variations of it as a Midjourney prompt with a Ad-lib version with keywords in backets like [backgrond], [time period], [style], etc. Always return your response inside of triple backtick code blocks. Do not provide explanation. Here is information about Midjourney as well as a prompt guide:
+      content: `You help create text prompts templates for an AI text-to-image software called Midjourney. You will take concepts and make 4 variations of it as a Midjourney prompt. Always return your response inside of triple backtick code blocks. Do not provide explanation. Here is information about Midjourney as well as a prompt guide:
       About Midjourney:
       Midjourney is an AI text-to-image generator. As the brand's website states, it aims to 'explore new mediums of thought and expanding the imaginative powers of the human species'. Midjourney asks you to input a worded prompt for an image, for example 'a fox wearing a top hat in the style of a Roald Dahl illustration' and in a few seconds, you'll be returned multiple attempts at this image in the form of a 4x4 image grid. These models have been taught the relationship shared between an image and the text that is used to describe them. The team behind Midjourney are now on the fifth iteration (V5). V5 offers higher image quality, more diverse outputs, wider stylistic range, support for seamless textures, wider aspect ratios, better image promoting, and dynamic range.
       Midjourney V5 Prompt Guide:
@@ -62,29 +58,35 @@ const generate = async draft => {
       --aspect must use whole numbers. Use 139:100 instead of 1.39:1.
       The aspect ratio impacts the shape and composition of a generated image.
       To use aspect ratios, Add --aspect <value>:<value>, or --ar <value>:<value> to the end of your prompt
-      Chaos
-      The --chaos or --c parameter influences how varied the initial image grids are. High --chaos values will produce more unusual and unexpected results and compositions. Lower --chaos values have more reliable, repeatable results.
-      --chaos accepts values 0-100.
-      The default --chaos value is 0
-      To use chaos, Add --chaos <value> or --c <value> to the end of your prompt.
-      Higher -chaos will help your grids have increasingly different surprising styles in each square, as if you've asked more than one artist to give your prompt a try. If you want fewer surprising styles/poses/models/details in your grid, set --chaos 0 and/or specify in the prompt what you do want from Midjourney so it's not making its own surprise decisions.
       Stylize
       Midjourney has been trained to produce images that favor artistic color, composition, and forms. The --stylize or --s parameter influences how strongly this training is applied. Low stylization values produce images that closely match the prompt but are less artistic. High stylization values create images that are very artistic but less connected to the prompt.
-      --stylize accepts values 0-1000
-      --stylize's default value is 100.
       To use stylize, Add --stylize <value> or --s <value> to the end of your prompt.
       Midjourney V5 Prompt Examples:
       Now that you know how to prompt in Midjourney V5, here are some example prompts that put all of that information together:
-      Zack Snyder's Wonderwoman portrait in chiaroscuro black & white graphite pencil, hard-key side light, golden armor, fierce eyes, moody, wet, rain, shiny, hyper realism, cinematic lighting --ar 4:7 --s 555 --c 3 --v 5
-      Cute, japanese, asian, kawaii, 8k, 18, kimono, girl, frontal shot, ultra detailed, ultra realistic, 85mm lens, f/ 1. 8, accent lighting, portrait, face, extreme close up, public street, day, skinny, hair ponytail, pastel, blonde, goddess --ar 9:16 --s 1000 --v 5
-      incredibly powerful Anime Girl, created by Katsuhiro Otomo + Rumiko Takahashi, Movie poster style, box office hit, a masterpiece of storytelling, main character center focus, monsters + mech creatures locked in combat, nuclear explosions paint sky, highly detailed 8k, 4k, intricate, detailed --ar 9:16 --v 5
-      Pointilism + impasto, diffrachromatic glowing ethereal light, Ilya Kuvshinov + Karmen Loh + Klimt + Akihiko Yoshida, gorgeous heavenly girl laying on her back in the moments after pure ecstasy, full body, skin --v 5 --c 12 --s 1000 --ar 2:3
-      Street style portrait of a female wearing a sunglass and a gray long-sleeve top in middle of foreground, background is brutalist style HDB apartments in Singapore, evening, shot on Kodak Portra 400 --ar 4:5 --s 250 --v 5
-      a close up of a person wearing a helmet, cyberpunk art, inspired by Tom Whalen, beautiful android woman, orange metal ears, vector artwork, martin ansin --v 5 --s 500 --ar 1:2 --chaos 9`,
+      
+      Zack Snyder's Wonderwoman portrait in chiaroscuro black & white graphite pencil, hard-key side light, golden armor, fierce eyes, moody, wet, rain, shiny, hyper realism, cinematic lighting
+      
+      Cute, japanese, asian, kawaii, 8k, 18, kimono, girl, frontal shot, ultra detailed, ultra realistic, 85mm lens, f/ 1. 8, accent lighting, portrait, face, extreme close up, public street, day, skinny, hair ponytail, pastel, blonde, goddess
+      incredibly powerful Anime Girl, created by Katsuhiro Otomo + Rumiko Takahashi, Movie poster style, box office hit, a masterpiece of storytelling, main character center focus, monsters + mech creatures locked in combat, nuclear explosions paint sky, highly detailed 8k, 4k, intricate, detailed --c 40
+      
+      Pointilism + impasto, diffrachromatic glowing ethereal light, Ilya Kuvshinov + Karmen Loh + Klimt + Akihiko Yoshida, gorgeous heavenly girl laying on her back in the moments after pure ecstasy, full body, skin --ar 16:9 --s 300
+      
+      Street style portrait of a female wearing a sunglass and a gray long-sleeve top in middle of foreground, background is brutalist style HDB apartments in Singapore, evening, shot on Kodak Portra
+      a close up of a person wearing a helmet, cyberpunk art, inspired by Tom Whalen, beautiful android woman, orange metal ears, vector artwork, martin ansin --ar 1:1 --c 100 --s 0.5`,
     },
     {
       role: 'user',
-      content: `Give 4 Midjourney prompts with adlib versions for this concept: "${draft}"`,
+      content: `Give 4 Midjourney prompts for this: "${draft}" 
+
+      **Prompt 1:**
+      \`\`\`/imagine prompt: [replace with prompt here]\`\`\`
+      **Prompt 2:**
+      \`\`\`/imagine prompt: [replace with prompt here]\`\`\`
+      **Prompt 3:**
+      \`\`\`/imagine prompt: [replace with prompt here]\`\`\`
+      **Prompt 4:**
+      \`\`\`/imagine prompt: [replace with prompt here]\`\`\`
+      `,
     },
   ]
 
